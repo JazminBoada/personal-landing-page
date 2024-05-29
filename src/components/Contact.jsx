c92d6de3a9a6fb89c7b2d5288431ef50078fb159
Jazmin <boadajazmin@gmail.com>
2024-05-29 16:01:12 -0300

    upgrade form

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,7 +1,46 @@
 import React from "react";
+import { useState } from "react";
 import { VscSend } from "react-icons/vsc";
 
 const Contact = () => {
+  const [formData, setFormData] = useState({
+    nombre: "",
+    email: "",
+    mensaje: "",
+  });
+
+  const [status, setStatus] = useState("");
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({
+      ...formData,
+      [name]: value,
+    });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const response = await fetch("https://formspree.io/f/mjvnqbbk", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(formData),
+    });
+
+    if (response.ok) {
+      setStatus("¡Mensaje enviado con éxito!");
+      setFormData({
+        nombre: "",
+        email: "",
+        mensaje: "",
+      });
+    } else {
+      setStatus("Error al enviar el mensaje.");
+    }
+  };
+
   return (
     <section className="bg-beige-light">
       <div className="flex flex-col items-center justify-center md:max-w-lg md:mx-auto">
@@ -12,19 +51,26 @@ const Contact = () => {
           </h1>
         </div>
 
-        <form className="flex flex-col space-y-2 mb-10 w-full px-8 pb-10">
+        <form
+          action="https://formspree.io/f/mjvnqbbk"
+          method="POST"
+          className="flex flex-col space-y-2 mb-10 w-full px-8 pb-10"
+        >
           <input
             className="p-2 outline-none overflow-hidden rounded-md shadow-sm"
+            name="nombre"
             type="text"
             placeholder="Nombre"
           />
           <input
             className="p-2 outline-none overflow-hidden rounded-md shadow-sm"
+            name="email"
             type="email"
             placeholder="Email"
           />
           <textarea
             className="resize-none h-40 p-2 outline-none overflow-hidden rounded-md shadow-sm"
+            name="mensaje"
             type="text"
             placeholder="Escribe tu mensaje"
           ></textarea>
